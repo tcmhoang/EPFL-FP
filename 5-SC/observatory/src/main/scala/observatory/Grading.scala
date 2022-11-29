09e7eec87f942678b2412ba228539a7358de0f84
Conrad Hoang <tcmhoang@outlook.com>
2022-11-29 16:56:59 +0700

    Implement great circle distance

--- 5-SC/observatory/src/main/scala/observatory/Grading.scala
+++ 5-SC/observatory/src/main/scala/observatory/Grading.scala
@@ -2,12 +2,10 @@ package observatory
 
 object Grading:
 
-  /**
-    * Configures which milestone / week should be graded. For instance,
-    * if you completed the milestone #2, you should set this value to `2`.
+  /** Configures which milestone / week should be graded. For instance, if you
+    * completed the milestone #2, you should set this value to `2`.
     *
-    * ''You'' have to update this value according to your progress
-    * in the project!
+    * ''You'' have to update this value according to your progress in the
+    * project!
     */
-  val milestone: Int = 1
-
+  val milestone: Int = 2
--- 5-SC/observatory/src/main/scala/observatory/Visualization.scala
+++ 5-SC/observatory/src/main/scala/observatory/Visualization.scala
@@ -5,34 +5,82 @@ import com.sksamuel.scrimage.pixels.Pixel
 import com.sksamuel.scrimage.metadata.ImageMetadata
 import com.sksamuel.scrimage.implicits.given
 import scala.collection.parallel.CollectionConverters.given
+import scala.math.{Pi, acos, cos, pow, sin, abs}
 
-/**
-  * 2nd milestone: basic visualization
+/** 2nd milestone: basic visualization
   */
+
+object VisualizationConstants {
+  val earthRadius = 6371.0
+  val minDistance = 1.0
+}
+
 object Visualization extends VisualizationInterface:
 
-  /**
-    * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
-    * @return The predicted temperature at `location`
+  /** @param temperatures
+    *   Known temperatures: pairs containing a location and the temperature at
+    *   this location
+    * @param location
+    *   Location where to predict the temperature
+    * @return
+    *   The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature =
+  def predictTemperature(
+      temperatures: Iterable[(Location, Temperature)],
+      location: Location
+  ): Temperature =
+    def toRad(degree: Double): Double = degree * Pi / 180
+
+    def dist(from: Location, to: Location): Double =
+      if (from == to) then 0
+      else if from.lat * -1 == to.lat && (180 - from.lon) * -1 == to.lon then Pi
+      else
+        val delta = acos(
+          sin(toRad(from.lat)) * sin(toRad(to.lat)) + cos(
+            toRad(from.lat)
+          ) * cos(
+            toRad(to.lat)
+          ) * cos(toRad(abs(from.lon - to.lon)))
+        )
+        VisualizationConstants.earthRadius * delta
+
+    def idw(
+        data: List[(Location, Temperature)],
+        numAcc: Double,
+        deAcc: Double
+    ): Temperature = ???
+
+    val minLoc =
+      temperatures.toArray.par.minBy((tuple) => dist(location, tuple._1))
+
+    if (dist(minLoc._1, location) < VisualizationConstants.minDistance) then
+      minLoc._2
+    else idw(temperatures.toList, 0, 0)
+
+  /** @param points
+    *   Pairs containing a value and its associated color
+    * @param value
+    *   The value to interpolate
+    * @return
+    *   The color that corresponds to `value`, according to the color scale
+    *   defined by `points`
+    */
+  def interpolateColor(
+      points: Iterable[(Temperature, Color)],
+      value: Temperature
+  ): Color =
     ???
 
-  /**
-    * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
-    * @return The color that corresponds to `value`, according to the color scale defined by `points`
+  /** @param temperatures
+    *   Known temperatures
+    * @param colors
+    *   Color scale
+    * @return
+    *   A 360×180 image where each pixel shows the predicted temperature at its
+    *   location
     */
-  def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color =
+  def visualize(
+      temperatures: Iterable[(Location, Temperature)],
+      colors: Iterable[(Temperature, Color)]
+  ): ImmutableImage =
     ???
-
-  /**
-    * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @return A 360×180 image where each pixel shows the predicted temperature at its location
-    */
-  def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): ImmutableImage =
-    ???
-
-
